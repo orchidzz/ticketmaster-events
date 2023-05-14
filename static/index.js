2c1c0d1ec9cbe9b530bdad1e1342459df5da41f6
Hang Nguyen <lannguyenhang@gmail.com>
2023-05-13 19:29:18 -0700

    fixed pagination buttons

--- static/index.js
+++ static/index.js
@@ -19,8 +19,9 @@ $("document").ready(function () {
     $("#date").attr("value", currentDate);
 
     $("#search-btn").on("click", function () {
-        // hide error display if search is attempted again
+        // hide error display and page navigation if search is attempted again
         $("#error").css("display", "none");
+        $("#page-nav").css("display", "none");
 
         req_location = $("#location").val();
         req_date = $("#date").val();
@@ -47,16 +48,19 @@ $("document").ready(function () {
                 if (response != null) {
                     // store response into cache
                     sessionStorage.setItem("events", JSON.stringify(response));
-                    sessionStorage.setItem("idx", "0");
+                    sessionStorage.setItem("idx", "-20");
                     generateCards(true);
+                    $("#page-nav").css("display", "initial");
                 } else {
                     // show some warning/error
                     $("#error").css("display", "flex");
+                    $("#page-nav").css("display", "none");
                 }
             },
             error: function (error) {
                 console.log("error getting events: ", error);
                 $("#error").css("display", "flex");
+                $("#page-nav").css("display", "none");
             },
         });
     });
@@ -73,31 +77,39 @@ $("document").ready(function () {
  * @param {boolean} next keep track of whether next button was clicked or not
  */
 function generateCards(next) {
+    // reset next and prev btns
+    $("#next-item").css("display", "initial");
+    $("#prev-item").css("display", "initial");
+
     let start = parseInt(sessionStorage.getItem("idx"));
+    let events = JSON.parse(sessionStorage.getItem("events"));
+    console.log(start);
     if (next) {
-        if (start + 20 < 100) {
+        if (start + 20 < events.length) {
             sessionStorage.setItem("idx", start + 20);
-            $("#next").attr("disabled", false);
-        } else {
-            //disable next
-            $("#next").attr("disabled", true);
-            return;
+            $("#next-item").css("display", "initial");
         }
     } else {
         if (start - 20 >= 0) {
             sessionStorage.setItem("idx", start - 20);
-            $("#prev").attr("disabled", false);
-        } else {
-            //disable prev
-            $("#prev").attr("disabled", true);
-            return;
+            $("#prev-item").css("display", "initial");
         }
     }
     // clear all cards first
     $("#events-container").empty();
     start = parseInt(sessionStorage.getItem("idx"));
-    let events = JSON.parse(sessionStorage.getItem("events"));
-    for (i = start; i < start + 20; ++i) {
+    console.log(start);
+
+    // disable next btn on last page
+    if (start + 20 >= events.length) {
+        $("#next-item").css("display", "none");
+    }
+    // disable prev btn on first page
+    if (start - 20 < 0) {
+        $("#prev-item").css("display", "none");
+    }
+
+    for (i = start; i < Math.min(start + 20, events.length); ++i) {
         $("#events-container").append(
             $("<div/>")
                 .addClass("card")
@@ -123,7 +135,6 @@ function generateCards(next) {
                                         events[i].currency +
                                         events[i].minPrice +
                                         "-" +
-                                        events[i].currency +
                                         events[i].maxPrice
                                 )
                         )
